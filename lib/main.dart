febcf7f1fdbcea2a90f6e5da34d99d0418464003
Hend <hend.jnnt@gmail.com>
2021-09-05 23:36:50 +0200

    color paletee

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,6 @@ void main() {
   runApp(MyApp());
 }
 
-const Map<int, Color> color = {
-  50: Color.fromRGBO(0, 161, 157, .1),
-  100: Color.fromRGBO(0, 161, 157, .2),
-  200: Color.fromRGBO(0, 161, 157, .3),
-  300: Color.fromRGBO(0, 161, 157, .4),
-  400: Color.fromRGBO(0, 161, 157, .5),
-  500: Color.fromRGBO(0, 161, 157, .6),
-  600: Color.fromRGBO(0, 161, 157, .7),
-  700: Color.fromRGBO(0, 161, 157, .8),
-  800: Color.fromRGBO(0, 161, 157, .9),
-  900: Color.fromRGBO(0, 161, 157, 1),
-};
-MaterialColor colorCustom = MaterialColor(0xFF00A19D, color);
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -27,7 +13,7 @@ class MyApp extends StatelessWidget {
       title: 'Chat App',
       debugShowCheckedModeBanner: false,
       theme:
-          ThemeData(primarySwatch: ctmColor(0), bottomAppBarColor: colorCustom),
+          ThemeData(primarySwatch: ctmColor(0), bottomAppBarColor: ctmColor(0)),
       home: SignInPage(),
     );
   }
