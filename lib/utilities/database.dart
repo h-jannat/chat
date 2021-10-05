9532f71e4dd258556af16b34e9984e5f8343346e
Hend Jannat <h.jannat@lamah.com>
2021-10-05 14:39:36 +0200

    .

--- lib/utilities/database.dart
+++ lib/utilities/database.dart
@@ -1,7 +1,8 @@
+import 'package:chat/utilities/signIn.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 
-class DatabaseController extends GetxController {
+class DatabaseController extends LoginController {
   getUserByUsername(String term) async {
     final result = await FirebaseFirestore.instance
         .collection("users")
@@ -16,5 +17,17 @@ class DatabaseController extends GetxController {
 
   uploadUserData(userData) {
     FirebaseFirestore.instance.collection("users").add(userData);
+  }
+
+  createChatRoom(String targetUserEmail) {
+    try{
+      //FirebaseFirestore.instance.collection("chat-rooms").document(id).setData(data).catch(err=>print(err))
+    FirebaseFirestore.instance.collection("chat-rooms").add({
+      "users": [user.email, targetUserEmail],
+      "chatRoomId": user.email + "-" + targetUserEmail
+    });
+    }catch(e){
+      print(e);
+    }
   }
 }
