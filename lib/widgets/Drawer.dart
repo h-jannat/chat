7aaf106a8f4bfbf54968d5489f9a3a031a53ea0e
Hend <hend.jnnt@gmail.com>
2021-11-24 23:25:27 +0200

    drawer elements

--- lib/widgets/Drawer.dart
+++ lib/widgets/Drawer.dart
@@ -1,9 +1,14 @@
+import 'package:chat/utilities/database.dart';
+import 'package:chat/widgets/ProfilePhoto.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
 import '../../colors.dart';
 
 class SideDrawer extends StatelessWidget {
   SideDrawer(this._loginController);
   final _loginController;
+  final _databaseController = Get.put(DatabaseController());
   Widget build(BuildContext context) {
     return Drawer(
       child: ListView(
@@ -11,30 +16,44 @@ class SideDrawer extends StatelessWidget {
         padding: EdgeInsets.zero,
         children: [
           DrawerHeader(
-              decoration: BoxDecoration(
-                color: ctmColor(1),
-              ),
-              child: Column(
-                children: [
-                  
-          
-                ],
-              )),
-          ListTile(
-            title: Text('Item 1'),
-            onTap: () {
-              // Update the state of the app.
-              // ...
-            },
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+            ),
+            child: Column(
+              children: [
+                ProfilePhoto('', 50),
+                Text(
+                  _databaseController.currentUser.username,
+                  style: TextStyle(fontSize: 17),
+                ),
+                Text(_databaseController.currentUser.email),
+              ],
+            ),
           ),
-          Divider(),
-          ListTile(
-            title: const Text('Item 2'),
-            onTap: () {
-              // Update the state of the app.
-              // ...
-            },
-          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text('Home'),
+                  onTap: () {
+                    // Update the state of the app.
+                    // ...
+                  },
+                ),
+                Divider(),
+                ListTile(
+                  title: const Text('Start new message'),
+                  onTap: () {
+                    // Update the state of the app.
+                    // ...
+                  },
+                ),
+                OutlinedButton(
+                    child: Text("Edit profile photo"), onPressed: () => {})
+              ],
+            ),
+          )
         ],
       ),
     );
